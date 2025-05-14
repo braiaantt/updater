826541d745966e80d5bc117b4c3b166313b05bb9
Braian Torres <brianntorress.s@gmail.com>
2025-05-14 17:30:18 -0300

    implement initConfig function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,3 +14,22 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::initConfig(){
+
+    QString appPath = QCoreApplication::applicationDirPath();
+    QFile file(appPath + "updater-config.json");
+    QByteArray fileBytes;
+
+    if(file.open(QFile::ReadOnly)){
+
+        fileBytes = file.readAll();
+        file.close();
+
+    } else {
+        qDebug()<<"error al abrir el archivo: "<<file.fileName();
+        return;
+    }
+
+    getServerInfo(fileBytes);
+
+}
