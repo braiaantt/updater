6d36a5edd37fa2c36a06a69e09ff727283388925
Braian Torres <brianntorress.s@gmail.com>
2025-05-16 02:55:59 -0300

    include QNetworkAccessManager, add manager variable and renamed execRequests function

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include "endpoint.h"
+#include <QtNetwork/QNetworkAccessManager>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -16,15 +17,19 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+public slots:
+    void latestAppVersionRequestFinished();
+
 private:
     Ui::MainWindow *ui;
     QVector<Endpoint> endpoints;
     QString hostName;
     QString mainAppVersion;
+    QNetworkAccessManager *manager;
 
     void initConfig();
     void getConfigJsonInfo(QByteArray&);
-    void execRequests();
+    void getLatestAppVersion();
 
 };
 #endif // MAINWINDOW_H
