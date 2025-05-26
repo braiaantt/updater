c2392847bc0196eef4893ea46939011eb6323f65
Braian Torres <brianntorress.s@gmail.com>
2025-05-25 22:39:40 -0300

    create class loadingitem

---
+++ loadingitem.cpp
@@ -0,0 +1,6 @@
+#include "loadingitem.h"
+
+LoadingItem::LoadingItem()
+{
+
+}
---
+++ loadingitem.h
@@ -0,0 +1,11 @@
+#ifndef LOADINGITEM_H
+#define LOADINGITEM_H
+
+
+class LoadingItem
+{
+public:
+    LoadingItem();
+};
+
+#endif // LOADINGITEM_H
