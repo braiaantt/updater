f36ca5b0df5982f190921ad1f686bc7bb7387bc2
Braian Torres <brianntorress.s@gmail.com>
2025-05-25 22:59:47 -0300

    add boundingRect and paint functions

--- loadingitem.h
+++ loadingitem.h
@@ -1,11 +1,18 @@
 #ifndef LOADINGITEM_H
 #define LOADINGITEM_H
 
+#include <QGraphicsItem>
+#include <QPainter>
 
-class LoadingItem
+class LoadingItem : QGraphicsItem
 {
 public:
     LoadingItem();
+
+    QRectF boundingRect() const override;
+
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *) override;
+
 };
 
 #endif // LOADINGITEM_H
