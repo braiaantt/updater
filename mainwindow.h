b6dcaafe59ead1196c8276118ff033744be6cf47
Braian Torres <brianntorress.s@gmail.com>
2025-05-26 20:20:34 -0300

    add currentReply variable and pushButtonCancel clicked signal

--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include "endpoint.h"
 #include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
 #include <QFileInfo>
 #include <QGraphicsScene>
 #include "loadingitem.h"
@@ -25,6 +26,11 @@ public slots:
     void latestAppVersionRequestFinished();
     void downloadNewUpdateRequestFinished();
 
+private slots:
+
+
+    void on_pushButtonCancel_clicked();
+
 private:
     Ui::MainWindow *ui;
     QVector<Endpoint> endpoints;
@@ -32,6 +38,7 @@ private:
     QString mainAppVersion;
     QString latestVersion;
     QNetworkAccessManager *manager;
+    QNetworkReply *currentReply;
     QGraphicsScene *scene;
     LoadingItem *spinner;
     QTimer *rotationTimer;
