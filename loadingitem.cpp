0c9f87118465fa450e46025a8a916393ab4a6d23
Braian Torres <brianntorress.s@gmail.com>
2025-05-26 00:39:44 -0300

    override boundingRect and paint functions

--- loadingitem.cpp
+++ loadingitem.cpp
@@ -4,3 +4,19 @@ LoadingItem::LoadingItem()
 {
 
 }
+
+QRectF LoadingItem::boundingRect() const {
+
+    return QRectF(-25, -25, 50, 50);
+
+}
+
+void LoadingItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *){
+
+    painter->setRenderHint(QPainter::Antialiasing);
+    painter->setPen(QPen(Qt::gray, 4));
+
+    QRectF rect(-20, -20, 40, 40);
+    painter->drawArc(rect, 0 * 16, 120 * 16);
+
+}
