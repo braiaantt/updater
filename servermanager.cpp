87925030624449663f8524c34874de253e697052
Braian Torres <brianntorress.s@gmail.com>
2025-05-27 17:58:17 -0300

    implement property setters

--- servermanager.cpp
+++ servermanager.cpp
@@ -4,3 +4,21 @@ ServerManager::ServerManager()
 {
 
 }
+//setters
+
+void ServerManager::setHostName(QString &_hostName){
+    hostName = _hostName;
+}
+
+void ServerManager::setGetVersionRoute(QString &_versionRoute){
+    getVersionRoute = _versionRoute;
+}
+
+void ServerManager::setDownloadVersionRoute(QString &_downloadVersionRoute){
+    downloadVersionRoute = _downloadVersionRoute;
+}
+
+void ServerManager::setSendLogRoute(QString &_sendLogRoute){
+    sendLogRoute = _sendLogRoute;
+}
+
