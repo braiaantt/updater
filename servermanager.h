9e4245b1cf0952d46398faaa84849f7a9b78235c
Braian Torres <brianntorress.s@gmail.com>
2025-05-27 17:39:40 -0300

    add properties to ServerManager for handling server URLs

--- servermanager.h
+++ servermanager.h
@@ -1,11 +1,18 @@
 #ifndef SERVERMANAGER_H
 #define SERVERMANAGER_H
 
+#include <QString>
 
 class ServerManager
 {
 public:
     ServerManager();
+
+private:
+    QString hostName;
+    QString getVersionRoute;
+    QString downloadVersionRooute;
+    QString sendLogRoute;
 };
 
 #endif // SERVERMANAGER_H
