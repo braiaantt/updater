f174f42481163a26368d5c527dd5606a9a537d2a
Braian Torres <brianntorress.s@gmail.com>
2025-05-28 19:16:35 -0300

    add deleteRecursively function

--- filemanager.h
+++ filemanager.h
@@ -14,6 +14,7 @@ public:
     QByteArray readFile(const QString &filePath);
     bool replaceOrCreateFile(const QString &filePath, const QByteArray &data);
     bool createFolder(const QString &dir);
+    bool deleteRecursively(const QString &dir);
     bool descompressZipFile(const QString &zipFilePath, const QString &destinationPath);
     bool searchFile(const QString &baseDir, const QFileInfo &fileSearched);
     void copyFile(const QString &source, const QString &target);
