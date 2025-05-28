320a62f5e6c5212f111e5baaf7ad4a26e35c5fc7
Braian Torres <brianntorress.s@gmail.com>
2025-05-28 15:39:51 -0300

    renamed searchFile parameter from 'path' to 'baseDir' and add copyFile function

--- filemanager.h
+++ filemanager.h
@@ -15,7 +15,8 @@ public:
     bool replaceOrCreateFile(const QString &filePath, const QByteArray &data);
     bool createFolder(const QString &dir);
     bool descompressZipFile(const QString &zipFilePath, const QString &destinationPath);
-    bool searchFile(const QString &path, const QFileInfo &fileSearched);
+    bool searchFile(const QString &baseDir, const QFileInfo &fileSearched);
+    void copyFile(const QString &source, const QString &target);
     QFileInfoList getDirEntries(const QString &dir);
 };
 
