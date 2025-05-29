81b299383a33b81958abab9bd84dd49326359208
Braian Torres <brianntorress.s@gmail.com>
2025-05-28 22:58:15 -0300

    add version and filename properties

--- mainappinfo.h
+++ mainappinfo.h
@@ -1,11 +1,16 @@
 #ifndef MAINAPPINFO_H
 #define MAINAPPINFO_H
 
+#include <QString>
 
 class MainAppInfo
 {
 public:
     MainAppInfo();
+
+private:
+    QString version;
+    QString fileName;
 };
 
 #endif // MAINAPPINFO_H
