8489b572ef94536d9446af766fe0048fc3e78e76
Braian Torres <brianntorress.s@gmail.com>
2025-05-28 23:06:17 -0300

    implement getters and setters

--- mainappinfo.cpp
+++ mainappinfo.cpp
@@ -4,3 +4,22 @@ MainAppInfo::MainAppInfo()
 {
 
 }
+
+//getters
+const QString& MainAppInfo::getFileName(){
+    return fileName;
+}
+
+const double& MainAppInfo::getVersion(){
+    return version;
+}
+
+//setters
+
+void MainAppInfo::setFileName(QString& _fileName){
+    fileName = _fileName;
+}
+
+void MainAppInfo::setVersion(double& _version){
+    version = _version;
+}
